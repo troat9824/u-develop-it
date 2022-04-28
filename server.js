5f0083660249f89469ec230d5504c153719f92b8
troat9824 <troat9824@gmail.com>
2022-04-27 22:22:55 -0500

    add query

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
+
 // Default response for any other request (Not found)
 app.use((req, res) => {
     res.status(404).end();
