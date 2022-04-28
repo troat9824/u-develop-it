99e3f5dfe31887322e5f84821b265843c0960960
troat9824 <troat9824@gmail.com>
2022-04-27 22:17:13 -0500

    add sql

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // Default response for any other request (Not found)
 app.use((req, res) => {
